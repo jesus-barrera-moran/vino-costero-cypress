007e0a8df388f619e28a8d165bb5156c3868c69f
jesus-barrera-moran <jesus.barrera.moran@gmail.com>
2024-10-23 16:29:48 -0600

    cu_06

--- cypress/e2e/cu_06/cp029.cy.js
+++ cypress/e2e/cu_06/cp029.cy.js
@@ -19,17 +19,27 @@ describe('Caso de Uso 6 - Modificación de Usuario', () => {
       cy.get('.ant-space > :nth-child(1) > span').click(); // Abrir menú de gestión de usuarios
       cy.get('.ant-dropdown-menu > :nth-child(3)').click(); // Seleccionar "Modificar usuario"
 
-      // 3. Seleccionar un usuario para modificar el rol
-      cy.get('[data-row-key="17"] > :nth-child(3) > .ant-select > .ant-select-selector').click(); // Abrir el dropdown de roles
+      // 3. Seleccionar la primera fila de usuario visible para modificar
+      cy.get('.ant-table-row > :nth-child(3)')
+        .last() // Tomar la primera fila visible
+        .within(() => {
+          // Modificar el rol del usuario
+          cy.get('.ant-select > .ant-select-selector').click(); // Abrir el dropdown de roles
+        });
+
       cy.get('.ant-select-item-option-content').contains(datos.rol).click(); // Seleccionar el nuevo rol
 
-      // 4. Cambiar el estado del usuario (habilitar/deshabilitar)
-      cy.get('[data-row-key="17"] > :nth-child(4) > .ant-switch').click(); // Cambiar el estado directamente
+      cy.get('.ant-table-row > :nth-child(4)')
+        .last() // Tomar la primera fila visible
+        .within(() => {
+          // Modificar el rol del usuario
+          cy.get('.ant-switch').click(); // Cambiar el estado directamente
+        });
 
-      // 5. Guardar los cambios
+      // 4. Guardar los cambios
       cy.get('.ant-card-body > :nth-child(3) > span').click(); // Guardar cambios
 
-      // 6. Verificar la notificación de éxito
+      // 5. Verificar la notificación de éxito
       cy.get('.ant-message-custom-content > :nth-child(2)').should('have.text', 'Cambios guardados exitosamente');
 
     });
